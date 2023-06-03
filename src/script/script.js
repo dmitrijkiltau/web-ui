40694dca9c48a9d7bb1ed97eac90496f4ef51a86
Dima <dmitrij@kiltau.com>
2023-06-03 19:04:25 +0200

    Initial commit

---
+++ src/script/script.js
@@ -0,0 +1,47 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('#main-header');
+  if (!header) return;
+
+  const mainMenu = document.querySelector('#main-menu');
+  const mainMenuItems = document.querySelectorAll('#main-menu .menu-item');
+  const flyout = document.querySelector('#main-flyout');
+  const flyoutItems = document.querySelectorAll('#main-flyout .flyout-item');
+  const flyoutOverlay = document.querySelector('#flyout-overlay');
+
+  if (mainMenu && mainMenuItems.length > 0 && flyout && flyoutItems.length > 0 && flyoutOverlay) {
+    initMainMenu(mainMenu, mainMenuItems, flyout, flyoutItems, flyoutOverlay);
+  }
+});
+
+function initMainMenu(mainMenu, mainMenuItems, flyout, flyoutItems, flyoutOverlay) {
+  for (const menuItem of mainMenuItems) {
+    menuItem.addEventListener('click', () => {
+      const identifier = menuItem.dataset.identifier;
+      (identifier) ? openFlyout(identifier) : closeFlyout();
+    }, { passive: true });
+  }
+
+  flyoutOverlay.addEventListener('click', closeFlyout, { passive: true });
+
+  function openFlyout(identifier) {
+    for (const menuItem of mainMenuItems) {
+      menuItem.classList.toggle('active', menuItem.dataset.identifier === identifier);
+    }
+
+    for (const flyoutItem of flyoutItems) {
+      flyoutItem.classList.toggle('active', flyoutItem.dataset.identifier === identifier);
+    }
+
+    flyout.classList.add('active');
+    flyoutOverlay.classList.add('active');
+  }
+
+  function closeFlyout() {
+    for (const menuItem of mainMenuItems) {
+      menuItem.classList.remove('active');
+    }
+
+    flyout.classList.remove('active');
+    flyoutOverlay.classList.remove('active');
+  }
+}
