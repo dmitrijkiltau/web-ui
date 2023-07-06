68ccda0c5922c2d13186656771b93c7970ae5ffd
Dima <dmitrij@kiltau.com>
2023-07-06 03:57:53 +0200

    Add indicators to ImageSlider

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,4 +1,4 @@
-import { For } from "solid-js";
+import { For, Show } from "solid-js";
 import { classnames } from "../../helper/classnames";
 import {
   addPassiveListener,
@@ -14,24 +14,46 @@ function ImageSlider(props) {
 
   return (
     <div class={classNames} id={id}>
-      <For each={images}>
-        {(image, i) => (
-          <img
-            src={image}
-            class="c-image"
-            alt={`Image ${i()}`}
-            data-index={i()}
-          />
-        )}
-      </For>
+      <div class="c-slider">
+        <div class="c-slides">
+          <For each={images}>
+            {(image, i) => (
+              <img
+                src={image}
+                class="c-image"
+                alt={`Image ${i()}`}
+                data-index={i()}
+              />
+            )}
+          </For>
+        </div>
+
+        <Show when={images.length > 1}>
+          <div class="c-indicators">
+            <For each={images}>
+              {(_, i) => (
+                <button
+                  class="c-indicator"
+                  data-index={i()}
+                  aria-label={`Slider indicator ${i()}`}
+                >
+                  <div class="c-line"></div>
+                </button>
+              )}
+            </For>
+          </div>
+        </Show>
+      </div>
     </div>
   );
 }
 
 function initImageSlider() {
   const slider = document.querySelector(".c-image-slider");
+  const slidesWrapper = slider?.querySelector(".c-slides");
   const slides = slider?.querySelectorAll(".c-image");
-  if (!slider || slides.length < 2) return;
+  const indicators = slider.querySelectorAll(".c-indicator");
+  if (!slider || !slidesWrapper || slides.length < 2) return;
 
   const duration = 3000;
   const slideDuration = 1000;
@@ -44,22 +66,60 @@ function initImageSlider() {
 
   slider.style.setProperty("--animation-duration", `${slideDuration}ms`);
   slides[0].classList.add("current");
-  startAutoPlay();
+  if (indicators) indicators[0].classList.add("current");
 
   const observerOptions = { root: null, threshold: 0.5 };
   const observer = new IntersectionObserver(observerCallback, observerOptions);
   observer.observe(slider);
 
-  function slideTo(nextSlide) {
+  if (indicators) {
+    for (const indicator of indicators) {
+      addPassiveListener(indicator, "click", handleIndicatorClick);
+    }
+  }
+
+  function observerCallback(entries, _) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        startAutoPlay();
+        addPassiveListener(window, "keydown", arrowKeyHandler);
+        addPassiveListener(slider, "mouseenter", stopAutoPlay);
+        addPassiveListener(slider, "mouseleave", startAutoPlay);
+        addPassiveListener(slidesWrapper, "touchstart", handleTouchStart);
+        addPassiveListener(slidesWrapper, "touchmove", handleTouchMove);
+        addPassiveListener(slidesWrapper, "touchend", handleTouchEnd);
+      } else {
+        removePassiveListener(window, "keydown", arrowKeyHandler);
+        removePassiveListener(slider, "mouseover", stopAutoPlay);
+        removePassiveListener(slider, "mouseout", startAutoPlay);
+        removePassiveListener(slidesWrapper, "touchstart", handleTouchStart);
+        removePassiveListener(slidesWrapper, "touchmove", handleTouchMove);
+        removePassiveListener(slidesWrapper, "touchend", handleTouchEnd);
+      }
+    });
+  }
+
+  function slideTo(index) {
     if (isTimeout) return;
-    const currentSlide = slider.querySelector(".current");
+    const currentSlide = slider.querySelector(".c-image.current");
+    const nextSlide = slides[index];
     if (!currentSlide || !nextSlide || currentSlide === nextSlide) return;
+
+    const currentIndicator = slider.querySelector(".c-indicator.current");
+    const nextIndicator = slider.querySelector(
+      `.c-indicator[data-index="${index}"]`
+    );
 
     stopAutoPlay();
     isTimeout = true;
     currentSlide.classList.remove("current");
     currentSlide.classList.add("prev");
     nextSlide.classList.add("current");
+
+    if (currentIndicator) {
+      currentIndicator.classList.remove("current");
+      nextIndicator.classList.add("current");
+    }
 
     setTimeout(() => {
       isTimeout = false;
@@ -74,7 +134,7 @@ function initImageSlider() {
     slideId = slideId + 1;
     if (slideId >= slides.length) slideId = 0;
     slider.classList.add("slide-left");
-    slideTo(slides[slideId]);
+    slideTo(slideId);
   }
 
   function slideToPrev() {
@@ -82,27 +142,7 @@ function initImageSlider() {
     slideId = slideId - 1;
     if (slideId < 0) slideId = slides.length - 1;
     slider.classList.add("slide-right");
-    slideTo(slides[slideId]);
-  }
-
-  function observerCallback(entries, _) {
-    entries.forEach(function (entry) {
-      if (entry.isIntersecting) {
-        addPassiveListener(window, "keydown", arrowKeyHandler);
-        addPassiveListener(slider, "mouseover", stopAutoPlay);
-        addPassiveListener(slider, "mouseout", startAutoPlay);
-        addPassiveListener(slider, "touchstart", handleTouchStart);
-        addPassiveListener(slider, "touchmove", handleTouchMove);
-        addPassiveListener(slider, "touchend", handleTouchEnd);
-      } else {
-        removePassiveListener(window, "keydown", arrowKeyHandler);
-        removePassiveListener(slider, "mouseover", stopAutoPlay);
-        removePassiveListener(slider, "mouseout", startAutoPlay);
-        removePassiveListener(slider, "touchstart", handleTouchStart);
-        removePassiveListener(slider, "touchmove", handleTouchMove);
-        removePassiveListener(slider, "touchend", handleTouchEnd);
-      }
-    });
+    slideTo(slideId);
   }
 
   function arrowKeyHandler(event) {
@@ -112,8 +152,10 @@ function initImageSlider() {
 
   function startAutoPlay() {
     isAutoPlay = true;
-    slideToNext();
-    timeout = setTimeout(startAutoPlay, duration);
+    timeout = setTimeout(() => {
+      slideToNext();
+      startAutoPlay();
+    }, duration);
   }
 
   function stopAutoPlay() {
@@ -134,6 +176,15 @@ function initImageSlider() {
     if (touchEndX > touchStartX) slideToPrev();
     touchStartX = 0;
     touchEndX = 0;
+  }
+
+  function handleIndicatorClick(e) {
+    if (isTimeout) return;
+    const index = e.target.dataset.index;
+    if (!index) return;
+    slider.classList.add(index > slideId ? "slide-left" : "slide-right");
+    slideId = parseInt(index);
+    slideTo(slideId);
   }
 }
 
