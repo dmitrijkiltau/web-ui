fecb6cba2896b3e6d1a2bbc525df688ac2bf61ec
Dima <dmitrij@kiltau.com>
2023-09-26 20:54:41 +0200

    Update examples

--- src/pages/Examples/Examples.jsx
+++ src/pages/Examples/Examples.jsx
@@ -11,66 +11,11 @@ function Examples() {
   useTranslation(locale);
 
   const sizes = ["full", "", "xl", "lg", "md", "sm", "xs"];
+  const columns = ["12", "2", "10", "4", "8", "6", "6", "8", "4", "10", "2"];
 
   return (
     <DefaultLayout>
-      <For each={sizes}>
-        {(size) => (
-          <Section width={size} className="mt-4 mb-4">
-            <div class="border-2 border-primary px-4 py-2">
-              Section {size ? `(${size})` : ""}
-            </div>
-          </Section>
-        )}
-      </For>
-
-      <For each={sizes.slice(2)}>
-        {(size) => (
-          <Section width={size} align="center" className="mb-4">
-            <div class="border-2 border-primary px-4 py-2">
-              Section {size ? `(center, ${size})` : ""}
-            </div>
-          </Section>
-        )}
-      </For>
-
-      <For each={sizes.slice(2)}>
-        {(size) => (
-          <Section width={size} align="right" className="mb-4">
-            <div class="border-2 border-primary px-4 py-2">
-              Section {size ? `(right, ${size})` : ""}
-            </div>
-          </Section>
-        )}
-      </For>
-
-      <Section className="mt-4 mb-4">
-        <Grid>
-          <Column xs="12" sm="6" md="4">
-            1
-          </Column>
-          <Column xs="12" sm="6" md="4">
-            2
-          </Column>
-          <Column xs="12" sm="6" md="4">
-            3
-          </Column>
-        </Grid>
-
-        <Grid>
-          <Column xs="12" sm="6" md="2">
-            1
-          </Column>
-          <Column xs="12" sm="6" md="6">
-            2
-          </Column>
-          <Column xs="12" sm="6" md="4">
-            3
-          </Column>
-        </Grid>
-      </Section>
-
-      <Section className="mt-4 mb-4">
+      <Section my="10">
         <h1>Headline 1</h1>
         <h2>Headline 2</h2>
         <h3>Headline 3</h3>
@@ -78,6 +23,62 @@ function Examples() {
         <h5>Headline 5</h5>
         <h6>Headline 6</h6>
         <p>Paragraph</p>
+      </Section>
+
+      <Section mb="4">
+        <h2>Sections</h2>
+      </Section>
+
+      <For each={sizes}>
+        {(size) => (
+          <Section width={size} mb="4">
+            <div class="border-2 border-primary px-4 py-2">
+              Section {size ? `(${size})` : ""}
+            </div>
+          </Section>
+        )}
+      </For>
+
+      <Section mt="10" mb="4">
+        <h2 class="text-center">Sections centered</h2>
+      </Section>
+
+      <For each={sizes.slice(2)}>
+        {(size) => (
+          <Section width={size} align="center" mb="4">
+            <div class="border-2 border-primary px-4 py-2">
+              Section {size ? `(center, ${size})` : ""}
+            </div>
+          </Section>
+        )}
+      </For>
+
+      <Section mt="10" mb="4">
+        <h2 class="text-end">Sections end</h2>
+      </Section>
+
+      <For each={sizes.slice(2)}>
+        {(size) => (
+          <Section width={size} align="right" mb="4">
+            <div class="border-2 border-primary px-4 py-2">
+              Section {size ? `(right, ${size})` : ""}
+            </div>
+          </Section>
+        )}
+      </For>
+
+      <Section mt="10" mb="8">
+        <h2>Grid with Columns</h2>
+
+        <Grid>
+          <For each={columns}>
+            {(size) => (
+              <Column sm={size} className="border-2 border-primary px-4 py-2">
+                {size}
+              </Column>
+            )}
+          </For>
+        </Grid>
       </Section>
     </DefaultLayout>
   );
